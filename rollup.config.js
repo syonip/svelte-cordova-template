7b9678475af56394aec0102cab88a6cf36250496
Johnny <syonip@gmail.com>
2019-06-30 18:20:29 +0300

    paths

--- rollup.config.js
+++ rollup.config.js
@@ -6,13 +6,15 @@ import { terser } from 'rollup-plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
 
+const cordovaFolder = 'src-cordova';
+
 export default {
 	input: 'src/main.js',
 	output: {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		file: 'public/bundle.js'
+		file: `${cordovaFolder}/www/bundle.js`
 	},
 	plugins: [
 		svelte({
@@ -21,7 +23,7 @@ export default {
 			// we'll extract any component CSS out into
 			// a separate file — better for performance
 			css: css => {
-				css.write('public/bundle.css');
+				css.write(`${cordovaFolder}/www/bundle.css`);
 			}
 		}),
 
@@ -35,7 +37,7 @@ export default {
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
-		!production && livereload('public'),
+		!production && livereload(`${cordovaFolder}/www`),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
