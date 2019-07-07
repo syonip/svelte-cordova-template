00609200df6c0adae74707e389013de2729a0c31
Johnny <syonip@gmail.com>
2019-07-07 19:16:56 +0300

    fixed html matching bug

--- src-cordova/hooks/before-prepare.js
+++ src-cordova/hooks/before-prepare.js
@@ -35,8 +35,8 @@ function updateConfigUrl(cordovaConfigPath, url) {
   let cordovaConfig = fs.readFileSync(cordovaConfigPath, 'utf-8')
   const lines = cordovaConfig.split(/\r?\n/g).reverse()
 
-  const contentIndex = lines.findIndex(line => line.match(/\s+<content/))
-  let allowNavigationIndex = lines.findIndex(line => line.match(/\s+<allow-navigation/))
+  const contentIndex = lines.findIndex(line => line.match(/\s*<content/))
+  let allowNavigationIndex = lines.findIndex(line => line.match(/\s*<allow-navigation/))
   if (contentIndex >= 0) {
     lines[contentIndex] = `    <content src="${url}" />`
   }
@@ -61,16 +61,16 @@ function generateIndexHtml(ctx) {
   let htmlContent = fs.readFileSync(`${publicFolder}/index.html`, 'utf-8')
   const lines = htmlContent.split(/\r?\n/g).reverse()
 
-  const bodyIndex = lines.findIndex(line => line.match(/\s+<\/body/))
+  const bodyIndex = lines.findIndex(line => line.match(/\s*<\/body/))
 
   let isIos = ctx.opts.platforms.includes('ios')
   const cordovaJsLocation = isIos ? 'bundle' : 'assets';
-  let cordovaJsScript = `        <script src="cdvfile://localhost/${cordovaJsLocation}/www/cordova.js"></script>`
+  let cordovaJsScript = `    <script src="cdvfile://localhost/${cordovaJsLocation}/www/cordova.js"></script>`
 
   if (bodyIndex >= 0) {
     lines.splice(bodyIndex + 1, 0, cordovaJsScript)
   } else {
-    console.error("couldn't find </body> tag in public/index.html")
+    console.error(`couldn't find </body> tag in ${publicFolder}/index.html`)
   }
   htmlContent = lines.reverse().join('\n')
   fs.writeFileSync('www/index.html', htmlContent)
